a2a8daa594883766d48011f243c4ac8bfa93fb1a
Stephen <stephenlallum@gmail.com>
2016-01-08 21:48:56 -1000

    Completed isTrue

--- ifelse.js
+++ ifelse.js
@@ -7,3 +7,16 @@ function youGetTaco(action) {
 	}
 }
 console.log(youGetTaco("eat"));
+
+
+
+//Function - isNumberGreaterThan
+//Write a function that takes two variables of type Number called first and secondrespectively. Return true if the first number is greater than the second.
+
+
+function isNumberGreaterThan(first, second) {
+	if (first > second) {
+		return true;
+	}
+}
+console.log(isNumberGreaterThan(9, 4));
