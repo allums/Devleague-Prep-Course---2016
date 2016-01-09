36eb43504c40f8a67fd1e881d80b3f1133636b3b
Stephen <stephenlallum@gmail.com>
2016-01-08 21:53:36 -1000

    isFalse completed

--- ifelse.js
+++ ifelse.js
@@ -1,5 +1,6 @@
 //Write a function that takes a single String parameter called action. If the value passed into our function is eathave the function return the String value EAT TACOS.
 
+
 function youGetTaco(action) {
 	var eatHave = "eat";
 	if (action === eatHave) {
@@ -20,3 +21,34 @@ function isNumberGreaterThan(first, second) {
 	}
 }
 console.log(isNumberGreaterThan(9, 4));
+
+
+
+//Function - isTrue
+//Write a function that takes in a Boolean value named val and have the function return whether the value is true.
+
+
+function isTrue(val) {
+	if (val === 300) {
+		return true;
+	}
+}
+console.log(isTrue(300));
+
+
+//Function - isFalse
+//Write a function that takes in a Boolean value named val and have the function return whether the value is false.
+
+function isFalse(val) {
+	if (val != 500) {
+		return false;
+	}
+}
+console.log(isFalse(300));
+
+
+
+
+
+
+
