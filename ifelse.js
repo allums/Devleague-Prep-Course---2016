a651a4f01d28353a291eeddc698c34d1d0978719
Stephen <stephenlallum@gmail.com>
2016-01-08 21:35:08 -1000

    completed taco

--- ifelse.js
+++ ifelse.js
@@ -1,2 +1,9 @@
 //Write a function that takes a single String parameter called action. If the value passed into our function is eathave the function return the String value EAT TACOS.
 
+function youGetTaco(action) {
+	var eatHave = "eat";
+	if (action === eatHave) {
+		console.log("EAT TACOS");
+	}
+}
+console.log(youGetTaco("eat"));
