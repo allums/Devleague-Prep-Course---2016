41dc9c4b6dbf0350bff041a7ba8fae71259c6541
Stephen <stephenlallum@gmail.com>
2016-01-15 18:05:39 -1000

    latest commit

--- moreforloops.js
+++ moreforloops.js
@@ -49,5 +49,39 @@ for (var i = oopsArray.length-1; i >= 0; i -= 1) {
  console.log(oopsArray[i]);
 }
 
+// Nap Schedule
+
+var isNapTime = false;
+var napSchedule = [false, false, true, false, true, true];
+
+function nap(schedule) {
+
+  if (schedule === true) {
+    console.log("ZzZzZzZ");
+  }
+  else {
+    console.log("gotta get to work!");
+    isNapTime = true;
+  }
+ }
+
+for (var i = 0; i < napSchedule.length; i++) {
+  nap(napSchedule[i]);
+}
+
+// Copy Of Arrays
+
+var copyOfValuesArray = [];
+var valuesArray = [99, 66, 829, 1941, 8, 76];
+
+function copyArray(originArray, destinationArray) {
+  for (var i = 0; i < valuesArray.length; i++) {
+    copyOfValuesArray.push(valuesArray[i]);
+  }
+}
+copyArray(copyOfValuesArray, valuesArray);
+
+console.log(copyOfValuesArray);
+console.log(valuesArray);
 
 
